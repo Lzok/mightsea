ebc3b5fb27b78303a8922f77d556751a389b1074
Leandro Echevarria <echevarrialeandro@gmail.com>
2022-05-26 19:21:21 -0300

    Backuped seeded database and storage, added to gitignore.

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,13 +1,12 @@
 import { signToken } from '@src/utils/auth';
 import { UserId, UserEntity } from '@src/@types/user';
 import { getUserById } from '@src/controllers/user';
-import logger from '@src/config/logger';
 
 export async function login(userId: UserId) {
 	const user = (await getUserById(userId)) as unknown as UserEntity;
 
 	const accessToken = await generateTokens(user.id);
-	logger.info(`Access Token ${accessToken}`);
+
 	return { accessToken, user };
 }
 
